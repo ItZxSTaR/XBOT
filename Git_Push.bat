43a9ec28ce28d87bb7bf3b4ace9c25861f3a52d8
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-29 22:39:35 +0530

    Add files via upload

---
+++ Git_Push.bat
@@ -0,0 +1,12 @@
+@echo off
+TITLE Github Quick-pushing 
+echo Pushing to branch: 
+git branch
+echo.
+set /p commit_title="Enter Commit title (pushes with you as author): "
+
+echo.
+git pull 
+git add *
+git commit -m "%commit_title%"
+git push
