15e244baf118d04c6a5b4ad255a10645d0776bdc
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-29 22:27:58 +0530

    Add files via upload

---
+++ AltronRobot/utils/functions.py
@@ -0,0 +1,10 @@
+from io import BytesIO
+
+from AltronRobot import aiohttpsession
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
